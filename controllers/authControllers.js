87aaad495b7965c42a578f53471fdd898c3955bb
Roshni <sparksroshni610@gmail.com>
2022-12-07 11:01:15 +0530

    cookie auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -15,9 +15,9 @@ const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
   req.session.cookie("jwt", token, {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+    // expires: new Date(
+    //   Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    // ),
     httpOnly: true,
     secure: req.secure || req.headers["x-forwarded-proto"] === "https",
   });
