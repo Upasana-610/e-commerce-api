1f482d59a8d8376aa9fa2cf9a37dcdf03dc789dd
Roshni <sparksroshni610@gmail.com>
2022-12-07 10:55:20 +0530

    cookie auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -14,7 +14,7 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
-  res.cookie("jwt", token, {
+  req.session.cookie("jwt", token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
