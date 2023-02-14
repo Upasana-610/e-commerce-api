4870e461a9ce2cfd7edc46d49cf9d7b5f5a3eac5
Roshni <sparksroshni610@gmail.com>
2023-02-15 01:07:51 +0530

    api link fixed

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -13,8 +13,8 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-
-  req.session.cookie("jwt", token, {
+  // console.log(token);
+  res.cookie("jwt", token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
