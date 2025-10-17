5fa8e3903dfbbe48c3c7d790d2518b3837ad0054
Upasana Pan <72752008+Upasana-610@users.noreply.github.com>
2025-10-17 18:09:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const server = app.listen(port, () => {
 process.on("unhandledRejection", (err) => {
   console.log("UNHANDLED REJECTION! 💥 Shutting down...");
   console.log("DATABASE:", DB);
-  console.log(err.name, err.message);
+  console.log(err);
   server.close(() => {
     process.exit(1);
   });
