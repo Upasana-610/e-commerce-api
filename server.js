4e184d81a52ea18ae2ae6d4c8b3f6082c2bd769d
Upasana Pan <72752008+Upasana-610@users.noreply.github.com>
2025-10-17 17:06:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const server = app.listen(port, () => {
 
 process.on("unhandledRejection", (err) => {
   console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log('MONGO_URI:', process.env.MONGO_URI);
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
